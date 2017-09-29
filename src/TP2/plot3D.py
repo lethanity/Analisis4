fb6486b797267929fee6eade10d1f7767eb48931
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2017-09-29 20:31:50 -0300

    Huge advances in 2D plotting technology

--- src/TP2/plot3D.py
+++ src/TP2/plot3D.py
@@ -18,10 +18,12 @@ def plot():
                     x_list.append(row[1])
                     y_list.append(row[2])
                     z_list.append(row[0])
-                k = k+1 '''
+                k = k+1 
+                
+'''
 
 
-def plot(x_val, y_val, z_val):
+def plot_3d(x_val, y_val, z_val):
     mpl.rcParams['legend.fontsize'] = 10
 
     fig = plt.figure()
@@ -30,3 +32,15 @@ def plot(x_val, y_val, z_val):
     ax.legend()
 
     plt.show()
+
+
+def plot_2d(x_val, y_val, x_label, y_label, title):
+    plt.plot(x_val, y_val)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.show()
+
+
+if __name__ == '__main__':
+    plot_2d([1, 2, 3, 4, 5], [2, 3, 4, 5, 6], 'x', 'y', 'x+1')
