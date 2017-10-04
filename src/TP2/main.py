78c08e4ee7393d7d2f234169190b8e6971cc3581
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2017-10-03 22:49:10 -0300

    pythonize tuple unpacking

--- src/TP2/main.py
+++ src/TP2/main.py
@@ -3,14 +3,8 @@ from TP2.plot import *
 
 
 if __name__ == '__main__':
-    points = get_interpolated_points()
-    heights = []
-    latitudes = []
-    longitudes = []
-    for point in points:
-        heights.append(point.height)
-        latitudes.append(point.latitude)
-        longitudes.append(point.longitude)
+    points = list(get_interpolated_points())
+    _, heights, latitudes, longitudes = zip(*points)  # python overload
 
     plot_2d(heights, longitudes, 'height', 'longitude')
     plot_2d(heights, latitudes, 'height', 'latitude')
