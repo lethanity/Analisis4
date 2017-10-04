b004652acf9b91d938f4b623375e166edad00dc3
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2017-10-03 21:33:03 -0300

    make interpolation in chunks to improve precision

--- src/TP2/interpolation.py
+++ src/TP2/interpolation.py
@@ -2,9 +2,11 @@ from sympy import *
 from collections import namedtuple
 import numpy
 import csv
+import pandas as pd
 
 POINTS_FILE = "geo_pos.csv"
 INTERPOLATED_POINTS_FILE = "geo_pos_interpolated.csv"
+LAGRANGE_LIMIT = 10
 
 
 def lagrange(points):
@@ -42,13 +44,32 @@ def lagrange(points):
 
 
 def interpolate_file():
-    points = numpy.genfromtxt(POINTS_FILE, dtype=numpy.float64, delimiter=',', skip_header=1)
-    latitude_by_height = lagrange(points[:, [0, 1]])
-    longitude_by_height = lagrange(points[:, [0, 2]])
+    data = pd.read_csv(POINTS_FILE)
+    row_num = data.shape[0]
     interpolated_points = []
-    for height in range(10000, -1, -1):
-        interpolated_points.append([height, latitude_by_height(height), longitude_by_height(height)])
+    for i in range(0, row_num, LAGRANGE_LIMIT - 1):
+        sliced_data = data.iloc[i:i+LAGRANGE_LIMIT]
+        latitude_by_height = numpy.column_stack((sliced_data.height, sliced_data.latitude))
+        longitude_by_height = numpy.column_stack((sliced_data.height, sliced_data.longitude))
+        first = sliced_data.height.iloc[0]
+        if sliced_data.shape[0] < LAGRANGE_LIMIT:
+            last = -1
+        else:
+            last = sliced_data.height.iloc[-1]
+        lat_by_height_fun = lagrange(latitude_by_height)
+        long_by_height_fun = lagrange(longitude_by_height)
+        for height in range(first, last, -1):
+            interpolated_points.append([height, lat_by_height_fun(height), long_by_height_fun(height)])
     return interpolated_points
+
+    # points = numpy.genfromtxt(POINTS_FILE, dtype=numpy.float64, delimiter=',', skip_header=1)
+    # print(points[:, [0, 1]])
+    # latitude_by_height = lagrange(points[:, [0, 1]])
+    # longitude_by_height = lagrange(points[:, [0, 2]])
+    # interpolated_points = []
+    # for height in range(10000, -1, -1):
+    #     interpolated_points.append([height, latitude_by_height(height), longitude_by_height(height)])
+    # return interpolated_points
 
 
 def generate_interpolated_file():
@@ -70,3 +91,39 @@ def get_non_interpolated_points():
         reader = csv.reader(infile)
         Data = namedtuple("Data", next(reader))
         return [Data(*value) for value in reader]
+
+
+if __name__ == '__main__':
+    generate_interpolated_file()
+    # data = pd.read_csv(POINTS_FILE)
+    # row_num = data.shape[0]
+    # interpolated_points = []
+    # for i in range(0, row_num, LAGRANGE_LIMIT - 1):
+    #     sliced_data = sliced_data = data.iloc[i:i+LAGRANGE_LIMIT]
+    #     latitude_by_height = numpy.column_stack((sliced_data.height, sliced_data.latitude))
+    #     longitude_by_height = numpy.column_stack((sliced_data.height, sliced_data.longitude))
+    #     first = sliced_data.height.iloc[0]
+    #     if sliced_data.shape[0] < LAGRANGE_LIMIT:
+    #         last = -1
+    #     else:
+    #         last = sliced_data.height.iloc[-1]
+    #     lat_by_height_fun = lagrange(latitude_by_height)
+    #     long_by_height_fun = lagrange(longitude_by_height)
+    #     for height in range(first, last, -1):
+    #         interpolated_points.append([height, lat_by_height_fun(height), long_by_height_fun(height)])
+    # return interpolated_points
+
+
+    # points = []
+    # with open(POINTS_FILE, newline="") as infile:
+    #     reader = csv.reader(infile)
+    #     next(reader)  # ignore col names
+    #     for values in reader:
+    #         points.extend(map(float, values))
+    # print(points)
+    # sliced_points = []
+    # for i in range(0, len(points), 9):
+    #     sliced_points.append(points[i:i+10])
+    # for i in sliced_points:
+    #     for height in range(i[len(i)-1], i[0] - 1, -1):
+    #         print(height)
--- src/TP2/main.py
+++ src/TP2/main.py
@@ -3,11 +3,9 @@ from TP2.plot import *
 
 
 if __name__ == '__main__':
-    print([0, 1, 2][:, [0, 1]])
-    # points = get_interpolated_points()
-    # heights = [float(point.height) for point in points]
-    # latitudes = [float(point.latitude) for point in points]
-    # print(latitudes)
-    # longitudes = [float(point.longitude) for point in points]
-    #
-    # plot_2d(heights, longitudes, 'height', 'longitude', '')
+    points = get_interpolated_points()
+    heights = [float(point.height) for point in points]
+    latitudes = [float(point.latitude) for point in points]
+    longitudes = [float(point.longitude) for point in points]
+
+    plot_3d(heights, longitudes, latitudes)
